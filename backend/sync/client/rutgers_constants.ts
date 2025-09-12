fdfc9cd13bada66b66f240cda9bf819584c4d022
jonnelin.leonardo <jonnelin.leonardo@gmail.com>
2025-09-12 18:21:35 +0000

    Add request constants

---
+++ backend/sync/client/rutgers_constants.ts
@@ -0,0 +1,11 @@
+/** The server port for making requests to the Rutgers servers. */
+export const SERVER_PORT = 443;
+
+/** The hostname for the Rutgers schedule of classes. */
+export const HOSTNAME = 'sis.rutgers.edu';
+
+/** The path for the Rutgers subjects data. */
+export const SUBJECTS_PATH = '/oldsoc/subjects.json';
+
+/** The path for the Rutgers courses data. */
+export const COURSES_PATH = '/oldsoc/courses.json';
