ea4aff6a063e8e7f1eeba4174f9b70142e35d3be
jonnelin.leonardo <jonnelin.leonardo@gmail.com>
2025-09-28 20:11:55 +0000

    Fix unit test Github workflow

--- backend/sync/jest.config.js
+++ backend/sync/jest.config.js
@@ -1,17 +1,17 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  preset: 'ts-jest/presets/default-esm',
-  testEnvironment: 'node',
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
   moduleNameMapper: {
-    '^(\.{1,2}/.*)\.js$': '$1',
+    "^(\.{1,2}/.*)\.js$": "$1",
   },
   transform: {
-    '^.+\.tsx?$': [
-      'ts-jest',
+    "^.+\.tsx?$": [
+      "ts-jest",
       {
         useESM: true,
       },
     ],
   },
-  testPathIgnorePatterns: ['/node_modules/', '/out/'],
+  testPathIgnorePatterns: ["/node_modules/", "/out/"],
 };
